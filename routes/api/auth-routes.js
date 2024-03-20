4fa13582ec533c2f99dac6ef4d2d842e703ca15b
VirLg <19virlg@gmail.com>
2024-03-20 22:22:19 +0200

    add sart files

---
+++ routes/api/auth-routes.js
@@ -0,0 +1,22 @@
+// import { Router } from 'express';
+// import controllerUser from '../../controllers/auth-controller.js';
+
+// const {
+//   signup,
+//   signin,
+//   getCurrent,
+//   logout,
+//   updateAvatar,
+//   verificationElasticEmail,
+//   resendEmailVerify,
+// } = controllerUser;
+
+// const authRouter = Router();
+// authRouter.get('/', getCurrent);
+
+// export default authRouter;
+app.get('/', async (req, res) => {    const [rows, fields] = await connection.query('SELECT * FROM abonents')
+res.json(rows);
+
+}
+    
